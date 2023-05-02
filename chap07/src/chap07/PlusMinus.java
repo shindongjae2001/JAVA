0325d3fd2507dd612ce98b3b418698c1f236c89c
shindongjae2001 <129822456+shindongjae2001@users.noreply.github.com>
2023-05-02 17:56:55 +0900

    Add files via upload

---
+++ chap07/src/chap07/PlusMinus.java
@@ -0,0 +1,14 @@
+package chap07;
+
+public class PlusMinus {
+	int plus,minus;
+	
+	public String plus(int x,int y) { //메소드
+		plus=x+y;
+		return "두 수의 합은" + plus;
+	}
+	public String minus(int x,int y) {
+		minus=x-y;
+		return "두 수의 차는 "+minus;
+	}
+}
---
+++ chap07/src/chap07/StudentTest.java
@@ -0,0 +1,16 @@
+package chap07;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student s1=new Student(); //객체 생성
+		System.out.println("s1 변수가 Student 객체 참조");
+		
+		Student s2=new Student(); //객체 생성
+		System.out.println("s2 변수가 또 다른 Student 객체 참조");
+
+	    //s1과 s2는 서로 다른 객체
+	}
+
+}
